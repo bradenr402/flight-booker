e0413cf017d88e5bb36e55e82205da0a05c05ba5
Braden <108199884+bradenr402@users.noreply.github.com>
2023-11-14 11:16:22 -0500

    Refactor to follow stimulus best practices https://www.betterstimulus.com

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -2,50 +2,49 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="passenger"
 export default class extends Controller {
-  static values = { "number": Number }
-  static targets = [ "numberPassengersElement", "label", "name", "email", "passengerBox" ]
+  static values = { numberOfPassengers: Number }
+  static targets = [ "template", "passenger", "passengerBox", "name", "email", "nameLabel", "emailLabel" ]
+
+  connect() {
+    console.log("numberOfPassengersValue:", this.numberOfPassengersValue)
+  }
 
   add() {
     if ("content" in document.createElement("template")) {
-      const number_of_passengers = ++this.numberPassengersElementTarget.value
+      this.numberOfPassengersValue++;
+      const number_of_passengers = this.numberOfPassengersValue;
 
-      console.log(number_of_passengers)
+      console.log(number_of_passengers);
 
-      const template = document.querySelector("#new_passenger")
-      const passenger_fields = document.querySelector("#passenger_fields")
+      const template = this.templateTarget;
+      const all_passenger_boxes = document.querySelectorAll('[data-passenger-target="passengerBox"');
+      const passenger_box = all_passenger_boxes[all_passenger_boxes.length - 1];
 
-      const clone = template.content.cloneNode(true)
-      passenger_fields.appendChild(clone)
+      const clone = template.content.cloneNode(true);
+      passenger_box.appendChild(clone);
 
-      const pname_name_attr = `booking[passengers_attributes][${number_of_passengers - 1}][name]`;
-      const pemail_name_attr = `booking[passengers_attributes][${number_of_passengers - 1}][email]`;
+      const pname_name_attr = `booking[passengers_attributes][${number_of_passengers}][name]`;
+      const pemail_name_attr = `booking[passengers_attributes][${number_of_passengers}][email]`;
 
-      const pname_id_attr = `booking_passengers_attributes_${number_of_passengers - 1}_name`;
-      const pemail_id_attr = `booking_passengers_attributes_${number_of_passengers - 1}_email`;
+      const pname_id_attr = `booking_passengers_attributes_${number_of_passengers}_name`;
+      const pemail_id_attr = `booking_passengers_attributes_${number_of_passengers}_email`;
 
-      this.nameTargets.slice(-1)[0].setAttribute("name", pname_name_attr)
-      this.emailTargets.slice(-1)[0].setAttribute("name", pemail_name_attr)
+      this.nameTargets[this.nameTargets.length - 1].setAttribute("name", pname_name_attr);
+      this.emailTargets[this.emailTargets.length - 1].setAttribute("name", pemail_name_attr);
 
-      this.nameTargets.slice(-1)[0].setAttribute("id", pname_id_attr)
-      this.emailTargets.slice(-1)[0].setAttribute("id", pemail_id_attr)
+      this.nameTargets[this.nameTargets.length - 1].setAttribute("id", pname_id_attr);
+      this.emailTargets[this.emailTargets.length - 1].setAttribute("id", pemail_id_attr);
 
-      let i = 1;
-      this.labelTargets.slice(-2).map((label) => {
-        if (i == 1) {
-          label.setAttribute("for", pname_id_attr)
-        } else {
-          label.setAttribute("for", pemail_id_attr)
-        }
-        i++;
-      });
+      this.nameLabelTargets[this.nameLabelTargets.length - 1].setAttribute("for", pname_id_attr);
+      this.emailLabelTargets[this.emailLabelTargets.length - 1].setAttribute("for", pemail_id_attr);
     }
   }
 
   remove() {
-    if (this.numberPassengersElementTarget.value > 0) {
-      this.numberPassengersElementTarget.value--;
-
-      this.passengerBoxTargets.slice(-1)[0].remove()
+    if (this.numberOfPassengersValue > 0) {
+      this.numberOfPassengersValue--;
+      let passenger_to_remove = this.passengerBoxTargets[this.passengerBoxTargets.length - 1];
+      passenger_to_remove.remove();
     }
   }
 }
